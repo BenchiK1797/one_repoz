637a5751b63541bc895b028bccd5daa023c0676b
BenchiK1797 <FenYa_11@mail.ru>
2023-10-03 20:00:07 +0300

    0310.py add new method delSecondElement

---
+++ main.py
@@ -0,0 +1,47 @@
+# def ploshad(a,b):
+#     return a*b
+#
+# a = int(input("введите высоту прямоугольника"))
+# b = int(input("введите ширину прямоугольника"))
+# print(f"площадь прямоугольника = {ploshad(a,b)}")
+import math
+
+
+# def ploshad(a,r) :
+#     return a*r**2
+# a = math.pi
+# r = int(input("введите радиус окружности"))
+# print(f"площадь круга = {ploshad(a,r)}")
+
+
+# import math
+# def ploshad(a,b,r):
+#      result = {}
+#      areaCircle = math.pi *r**2
+#      areaSquare = a*b
+#      result.update({"Площадь крвадрата":areaSquare})
+#      result.update({"Площадь крга":areaCircle})
+#      return result
+# a = int(input("введите высоту прямоугольника"))
+# b = int(input("введите ширину прямоугольника"))
+# r = int(input("введите радиус круга"))
+# for key,item in ploshad(a,b,r).items():
+#     print(f"{key}:{item}")
+
+
+import random
+def getHealth():
+
+    health = 100
+    i = 0
+    while True:
+        i+=1
+        a = random.randint(1, 20)
+        b = random.randint(1, 20)
+        if a > b:
+            health -= 20
+        if health <= 0:
+            print("здоровье законилось",i)
+            break
+
+getHealth()
