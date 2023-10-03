b8d0fde544bd54734b4da7da18a6042a8b0c070a
BenchiK1797 <FenYa_11@mail.ru>
2023-10-03 21:48:28 +0300

    весь урок

--- 0310.py
+++ 0310.py
@@ -1,5 +1,29 @@
-def delSecondElement(array:list):
+# def delSecondElement(array:list):
+#
+#     return  array[::2]# по умолчанию [начало листа:конец листа:шаг]
+# print(delSecondElement([1,2,3,4,5,6,7,8,9]))
 
-    return  array[::2]# по умолчанию [начало листа:конец листа:шаг]
-print(delSecondElement([1,2,3,4,5,6,7,8,9]))
-select
+# def fibonachi(n):
+#     if n==0 or n==1:
+#         return  n
+#     print(fibonachi(n-1)+fibonachi(n-2))
+#     return fibonachi(n-1)+fibonachi(n-2)
+# print(fibonachi(7))
+
+
+# def vozvrat(n):
+#     if n==-2 or n==-3:
+#         return n
+#     print(vozvrat(n-2) + vozvrat(n-1))
+#     return vozvrat(n-2)+vozvrat(n-1)
+# print(vozvrat(1))
+
+
+import math
+a = int(input("ведите количество скторов"))
+r = int(input("задайте радиус круга"))
+def ploshadSect():
+    ploshad = (math.pi*r**2)/a
+    return ploshad
+print(ploshadSect())
+
